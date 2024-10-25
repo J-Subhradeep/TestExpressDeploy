d08525f6dbae128bd5b0aa04aa0d8d3456959f0c
J-Subhradeep <subhradeeppal30@gmail.com>
2024-10-25 21:09:16 +0530

    update code

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000; // You can change the port if needed
 
 // Simple GET endpoint
-app.get('/', (req, res) => {
-  res.send('Hello, welcome to my simple Express.js server!');
+app.get("/", (req, res) => {
+  res.send("Hello, welcome to my simple Express.js server update!");
 });
 
 // Start the server
