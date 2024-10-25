b48927edb7490a795a42c090df3392978ea3eaff
J-Subhradeep <subhradeeppal30@gmail.com>
2024-10-25 20:45:37 +0530

    new project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can change the port if needed
+
+// Simple GET endpoint
+app.get('/', (req, res) => {
+  res.send('Hello, welcome to my simple Express.js server!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
